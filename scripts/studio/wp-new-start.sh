9d1a453dde63b1f87c6833a6641cd8f9f8a0afe2
Alan Thatcher <alanwthatcher@gmail.com>
2019-03-05 19:49:24 -0500

    fixes and updates

--- scripts/studio/wp-new-start.sh
+++ scripts/studio/wp-new-start.sh
@@ -3,7 +3,7 @@ source results/demo.env
 hab config apply mysql.default $(date +%s) scripts/studio/mysql-wordpress-config.toml
 hab svc load $pkg_mysql5717
 
-if [ "$1" == 'nodbsetup' ]; then
+if [ "$1" == 'nodbstage' ]; then
   hab config apply wordpress.default $(date +%s) scripts/studio/wp-nodbrestore.toml
 fi
 
--- scripts/studio/wp-old-start.sh
+++ scripts/studio/wp-old-start.sh
@@ -3,7 +3,7 @@ source results/demo.env
 hab config apply mysql.default $(date +%s) scripts/studio/mysql-wordpress-config.toml
 hab svc load $pkg_wp_mysql
 
-if [ "$1" == 'nodbsetup' ]; then
+if [ "$1" == 'nodbstage' ]; then
   hab config apply wordpress.default $(date +%s) scripts/studio/wp-nodbrestore.toml
 fi
 
